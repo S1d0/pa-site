1615b9bb26f3bcaabd814c66517ac839629beaa0
sebastian <sidzkowski@softwaregoodies.org>
2024-04-23 13:05:49 +0200

    Added img gallery

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -86,3 +86,10 @@ export type InvoiceForm = {
   amount: number;
   status: 'pending' | 'paid';
 };
+
+
+export type ImageDesc = {
+  title: string;
+  desc: string;
+  href: string;
+}
--- app/ui/home/showcase.tsx
+++ app/ui/home/showcase.tsx
@@ -1,17 +1,26 @@
+'use client';
+
 import Link from 'next/link';
+import ImageContainer from './ImgContainer';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export default function Showcase() {
+  const [selected, setSelected] = useState('');
+
+  const toggleSelected = (key: string) => {
+    selected != key ? setSelected(key) : setSelected('');
+  };
+
   return (
     <section id="showcase" className="relative m-2 px-2 sm:my-16 sm:px-12">
-      <div className="m-auto flex flex-col items-center">
-        <div>
+      <div className="flex flex-col">
+        {/* Header and Navigation */}
+        <div className='flex flex-col items-center'>
           <h1 className="flex py-4 text-2xl font-bold text-stone-800 drop-shadow-2xl sm:flex md:text-5xl">
             Poznaj nasze realizacje
           </h1>
-        </div>
-
-        {/* Desktop Section */}
-        <div className="mb-4 hidden gap-4 sm:flex">
+          <div className="mb-4 hidden gap-4 sm:flex">
           <Link
             href="#"
             className="p-2 font-semibold leading-6 text-stone-800 hover:rounded-lg hover:bg-stone-800 hover:text-white"
@@ -47,96 +56,84 @@ export default function Showcase() {
             Łazienka
           </Link>
         </div>
+        </div>
+
+        {/* Gallery preview */}
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-6">
-          <div className="group relative overflow-hidden rounded-lg sm:col-span-3 sm:cursor-pointer">
-            <img
-              className="easy-in h-96 w-full max-w-full rounded-lg object-cover object-center brightness-50 transition-transform duration-500 group-hover:scale-110"
-              src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80"
-              alt="gallery-photo"
+          <div
+            className={clsx(
+              selected === 'home'
+                ? 'relative overflow-hidden col-span-full'
+                : 'relative overflow-hidden sm:col-span-3',
+              selected !== 'home' && selected !== '' && 'hidden',
+            )}
+            onClick={() => toggleSelected('home')}
+          >
+            <ImageContainer
+              desc="- projekt arch. Emili Zajkowskiej"
+              title="Dom"
+              href="https://images.pexels.com/photos/2089698/pexels-photo-2089698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             />
-            <div className="absolute inset-0 flex flex-col items-center justify-center align-middle opacity-50 transition-opacity duration-200 ease-in group-hover:opacity-100">
-              <h2 className="text-xl font-bold text-stone-200 first-letter:text-center sm:pb-4 sm:text-4xl">
-                Dom
-              </h2>
-              <p
-                className="block text-center  text-base font-bold text-stone-200 sm:text-xl
-              "
-              >
-                - projekt arch. Emili Zajkowskiej
-              </p>
-            </div>
           </div>
-          <div className="group relative overflow-hidden rounded-lg sm:col-span-3 sm:cursor-pointer">
-            <img
-              className="easy-in h-96 w-full max-w-full rounded-lg object-cover object-center brightness-50 transition-transform duration-500 group-hover:scale-110"
-              src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80"
-              alt="gallery-photo"
+          <div
+            className={clsx(
+              selected === 'flat'
+                ? 'relative overflow-hidden sm:col-span-6'
+                : 'relative overflow-hidden sm:col-span-3',
+              selected !== 'flat' && selected !== '' && 'hidden',
+            )}
+            onClick={() => toggleSelected('flat')}
+          >
+            <ImageContainer
+              desc="- projekt arch. Patrycji Zajkowskiej"
+              title="Mieszkanie"
+              href="https://images.pexels.com/photos/7015153/pexels-photo-7015153.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             />
-            <div className="absolute inset-0 flex flex-col items-center justify-center align-middle opacity-50 transition-opacity duration-200 ease-in group-hover:opacity-100">
-              <h2 className="text-xl font-bold text-stone-200 first-letter:text-center sm:pb-4 sm:text-4xl">
-                Mieszkanie
-              </h2>
-              <p
-                className="block text-center  text-base font-bold text-stone-200 sm:text-xl
-              "
-              >
-                - projekt arch. Patrycji Zajkowskiej
-              </p>
-            </div>
           </div>
-          <div className="group relative overflow-hidden rounded-lg sm:col-span-2 sm:cursor-pointer">
-            <img
-              className="easy-in h-96 w-full max-w-full rounded-lg object-cover object-center brightness-50 transition-transform duration-500 group-hover:scale-110"
-              src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80"
-              alt="gallery-photo"
+          <div
+            className={clsx(
+              selected === 'saloon'
+                ? 'relative overflow-hidden sm:col-span-6'
+                : 'relative overflow-hidden sm:col-span-2',
+              selected !== 'saloon' && selected !== '' && 'hidden',
+            )}
+            onClick={() => toggleSelected('saloon')}
+          >
+            <ImageContainer
+              title="Salon"
+              desc="- projekt arch. Emili Zajkowskiej"
+              href="https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             />
-            <div className="absolute inset-0 flex flex-col items-center justify-center align-middle opacity-50 transition-opacity duration-200 ease-in group-hover:opacity-100">
-              <h2 className="text-xl font-bold text-stone-200 first-letter:text-center sm:pb-4 sm:text-4xl">
-                Salon
-              </h2>
-              <p
-                className="block text-center  text-base font-bold text-stone-200 sm:text-xl
-              "
-              >
-                - projekt arch. Anny Zajkowskiej
-              </p>
-            </div>
           </div>
-          <div className="group relative overflow-hidden rounded-lg sm:col-span-2 sm:cursor-pointer">
-            <img
-              className="easy-in h-96 w-full max-w-full rounded-lg object-cover object-center brightness-50 transition-transform duration-500 group-hover:scale-110"
-              src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80"
-              alt="gallery-photo"
+          <div
+            className={clsx(
+              selected === 'kitchen'
+                ? 'relative overflow-hidden sm:col-span-6'
+                : 'relative overflow-hidden sm:col-span-2',
+              selected !== 'kitchen' && selected !== '' && 'hidden',
+            )}
+            onClick={() => toggleSelected('kitchen')}
+          >
+            <ImageContainer
+              title="Kuchnia"
+              desc="- projekt arch. Emili Zajkowskiej"
+              href="https://images.pexels.com/photos/1080721/pexels-photo-1080721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             />
-            <div className="absolute inset-0 flex flex-col items-center justify-center align-middle opacity-50 transition-opacity duration-200 ease-in group-hover:opacity-100">
-              <h2 className="sm:pb-4 text-xl font-bold text-stone-200 first-letter:text-center sm:text-4xl">
-                Kuchnia
-              </h2>
-              <p
-                className="block text-center  text-base font-bold text-stone-200 sm:text-xl
-              "
-              >
-                - projekt arch. Lorem Ipsum
-              </p>
-            </div>
           </div>
-          <div className="group relative overflow-hidden rounded-lg sm:col-span-2 sm:cursor-pointer">
-            <img
-              className="easy-in h-96 w-full max-w-full rounded-lg object-cover object-center brightness-50 transition-transform duration-500 group-hover:scale-110"
-              src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80"
-              alt="gallery-photo"
+          <div
+            className={clsx(
+              selected === 'bath'
+                ? 'relative overflow-hidden sm:col-span-6'
+                : 'relative overflow-hidden sm:col-span-2',
+              selected !== 'bath' && selected !== '' && 'hidden',
+            )}
+            onClick={() => toggleSelected('bath')}
+          >
+            <ImageContainer
+              title="Łazienka"
+              desc="- projekt arch. Emili Zajkowskiej"
+              href="https://images.pexels.com/photos/1910472/pexels-photo-1910472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             />
-            <div className="absolute inset-0 flex flex-col items-center justify-center align-middle opacity-50 transition-opacity duration-200 ease-in group-hover:opacity-100">
-              <h2 className="text-xl font-bold text-stone-200 first-letter:text-center sm:pb-4 sm:text-4xl">
-                Łazienka
-              </h2>
-              <p
-                className="block text-center  text-base font-bold text-stone-200 sm:text-xl
-              "
-              >
-                - projekt arch. Lorem Ipsunm
-              </p>
-            </div>
           </div>
         </div>
       </div>
